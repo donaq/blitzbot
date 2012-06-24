0c3066477a16cfb387bac514d47278a88be86507
Adrian Quek <adrianqsk@gmail.com>
2012-06-24 16:16:36 +0800

    Added more colors to the color map.

--- colors.py
+++ colors.py
@@ -2,6 +2,7 @@ colormap = {
 	(249, 26, 54):"r", # red plain
 	(250, 28, 55):"r", # red flame
 	(250, 17, 33):"r", # red x2
+	(236, 16, 31):"r", # red x3
 	(224, 224, 224):"w", # white plain
 	(225, 225, 224):"w", # white flame
 	(209, 209, 209):"w", # white x2
@@ -9,13 +10,21 @@ colormap = {
 	(230, 101, 33):"o", # orange plain
 	(231, 103, 34):"o", # orange flame
 	(250, 124, 43):"o", # orange x2
+	(236, 117, 40):"o", # orange x3
 	(16, 139, 254):"b", # blue plain
+	(20, 141, 254):"b", # blue flame
+	(40, 145, 236):"b", # blue x3
 	(254, 245, 35):"y", # yellow plain
 	(255, 246, 36):'y', # yellow flame
+	(243, 239, 0):"y", # yellow x2
+	(229, 225, 0):'y', # yellow x3
 	(186, 145, 34):"y", # yellow coin
 	(239, 15, 239):"p", # purple plain
 	(240, 21, 239):"p", # purple flame
 	(216, 18, 215):'p', # purple x2
+	(204, 17, 203):'p', # purple x3
 	(16, 164, 33):"g", # green plain
 	(18, 165, 34):"g", # green flame
+	(0, 193, 12):"g", # green x2
+	(0, 182, 12):"g", # green x3
 }
